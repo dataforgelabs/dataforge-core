011cc99fcc04042b0712e02356eccd35910866c9
Vadim Orlov <vorlov@dataforgelabs.com>
2024-04-25 15:45:46 -0700

    stop subprocesses, _exit to prevent pyspark atexit handler printing SUCCESS

--- cli/dataforge/main.py
+++ cli/dataforge/main.py
@@ -1,3 +1,7 @@
+import os
+import signal
+import psutil
+
 from .importProject import ImportProject
 from .mainConfig import MainConfig
 
@@ -9,6 +13,19 @@ def main():
         imp.load()
     if conf.run_path:
         conf.databricks.run(conf.run_path)
+
+    current_process = psutil.Process()
+    children = current_process.children(recursive=True)
+
+    #  terminate child processes
+    if len(children) > 0:
+        for p in children:
+            try:
+                p.send_signal(signal.SIGTERM)
+            except psutil.NoSuchProcess:
+                pass
+        psutil.wait_procs(children, timeout=5)
+    os._exit(os.EX_OK)
 
 
 if __name__ == '__main__':
