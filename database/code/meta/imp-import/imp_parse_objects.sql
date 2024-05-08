90608f24ad58f82814a0f1f548ef35cc3e008818
databasecorescript <jswanson@dataforgelabs.com>
2024-05-08 21:34:01 +0000

    import fixes

--- database/code/meta/imp-import/imp_parse_objects.sql
+++ database/code/meta/imp-import/imp_parse_objects.sql
@@ -17,7 +17,7 @@ BEGIN
         name =  CASE WHEN object_type IN ('source' ,'output', 'group', 'token') THEN p.body->>(object_type || '_name')
                 WHEN object_type IN ('output_template','source_template') THEN p.body->>'object_name'
         ELSE p.body->>'name' END,
-        hash = md5(io.body_text)
+        hash = md5(p.body::text)
     FROM parsed p WHERE p.import_object_id = io.import_object_id;
 
     INSERT INTO log.actor_log (log_id, message, actor_path, severity, insert_datetime)
